de7a611382daf44187119cd1a23b5badce84db4b
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2017-05-10 10:14:28 -0500

    added initial content

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const eslint = require('gulp-eslint');
+const mocha = require('gulp-mocha');
+const istanbul = require('gulp-istanbul');
+
+gulp.task('default', function() {
+  //Linting
+  gulp.src(["src/**/*.js", "test/**/*.js"])
+    .pipe(eslint())
+    .pipe(eslint.format())
+  // Babel src
+  gulp.src("src/**/*.js")
+    .pipe(babel())
+    .pipe(istanbul({includeUntested:true}))
+    .pipe(istanbul.hookRequire())
+    .pipe(gulp.dest("dist/babel-src"));
+  // Babel test
+  gulp.src("test/**/*.js")
+    .pipe(babel())
+    .pipe(gulp.dest("./dist/babel-test")).on('finish', () => {
+       gulp.src("./dist/babel-test/**/*.js")
+       .pipe(mocha())
+       .pipe(istanbul.writeReports("./dist/coverage"))
+    });
+});
---
+++ src/foo.js
@@ -0,0 +1,4 @@
+
+function values (a,b,c) {
+    return a + b + c;
+}
